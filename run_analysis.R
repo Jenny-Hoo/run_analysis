36580e4f97c9d281739215c0c79af412f2237f48
Jenny Hoo <15926559016@163.com>
2020-08-09 00:17:25 +0800

    updated

---
+++ run_analysis.R
@@ -0,0 +1,38 @@
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activity <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+X <- rbind(x_train, x_test)
+Y <- rbind(y_train, y_test)
+Subject <- rbind(subject_train, subject_test)
+Merged_Data <- cbind(Subject, Y, X)
+
+Tidy_data <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
+
+Tidy_data$code <- activity[Tidy_data$code, 2]
+
+names(Tidy_data)[2] = "activity"
+names(Tidy_data)<-gsub("Acc", "Accelerometer", names(Tidy_data))
+names(Tidy_data)<-gsub("Gyro", "Gyroscope", names(Tidy_data))
+names(Tidy_data)<-gsub("BodyBody", "Body", names(Tidy_data))
+names(Tidy_data)<-gsub("Mag", "Magnitude", names(Tidy_data))
+names(Tidy_data)<-gsub("^t", "Time", names(Tidy_data))
+names(Tidy_data)<-gsub("^f", "Frequency", names(Tidy_data))
+names(Tidy_data)<-gsub("tBody", "TimeBody", names(Tidy_data))
+names(Tidy_data)<-gsub("-mean()", "Mean", names(Tidy_data), ignore.case = TRUE)
+names(Tidy_data)<-gsub("-std()", "STD", names(Tidy_data), ignore.case = TRUE)
+names(Tidy_data)<-gsub("-freq()", "Frequency", names(Tidy_data), ignore.case = TRUE)
+names(Tidy_data)<-gsub("angle", "Angle", names(Tidy_data))
+names(Tidy_data)<-gsub("gravity", "Gravity", names(Tidy_data))
+
+Final_data <- Tidy_data %>%
+  group_by(subject, activity) %>%
+  summarise_all(mean)
+write.table(Final_data, "Final_data.txt", row.name=FALSE)
+
+str(Final_data)
